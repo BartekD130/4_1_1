2029c448df436281b790a0f6a63c91ab9e32fc0d
BartekD130 <bartosz.drwal.bd@gmail.com>
2025-12-08 10:58:16 +0000

    Commit4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,62 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    try {
-      Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Service service = new Service();
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
-      }
-    } catch (IOException e) {
+        int wybor = 0;
 
+        while (wybor != 3) {
+            System.out.println("\n=== MENU ===");
+            System.out.println("1 - Dodaj studenta");
+            System.out.println("2 - Wyświetl listę studentów");
+            System.out.println("3 - Wyjście");
+            System.out.print("Wybierz opcję: ");
+
+            wybor = scanner.nextInt();
+            scanner.nextLine(); // czyszczenie bufora
+
+            switch (wybor) {
+                case 1:
+                    System.out.print("Podaj imię: ");
+                    String imie = scanner.nextLine();
+
+                    System.out.print("Podaj wiek: ");
+                    int wiek = scanner.nextInt();
+                    scanner.nextLine();
+
+                    try {
+                        service.addStudent(new Student(imie, wiek));
+                        System.out.println("Dodano studenta!");
+                    } catch (IOException e) {
+                        System.out.println("Błąd zapisu do pliku!");
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        Collection<Student> students = service.getStudents();
+                        System.out.println("\n=== LISTA STUDENTÓW ===");
+                        for (Student s : students) {
+                            System.out.println(s.ToString());
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Błąd odczytu pliku!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Zakończono program.");
+                    break;
+
+                default:
+                    System.out.println("Nie ma takiej opcji!");
+            }
+        }
     }
-  }
 }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,24 +1,23 @@
 import java.util.Collection;
 import java.util.ArrayList;
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 public class Service {
-
   public void addStudent(Student student) throws IOException {
-    var f = new FileWriter("db.txt", true);
-    var b = new BufferedWriter(f);
+    FileWriter f = new FileWriter("db.txt", true);
+    BufferedWriter b = new BufferedWriter(f);
     b.append(student.ToString());
     b.newLine();
     b.close();
   }
-
   public Collection<Student> getStudents() throws IOException {
-    var ret = new ArrayList<Student>();
-    var f = new FileReader("db.txt");
-    var reader = new BufferedReader(f);
+    ArrayList<Student> ret = new ArrayList<Student>();
+    FileReader f = new FileReader("db.txt");
+    BufferedReader reader = new BufferedReader(f);
     String line = "";
     while (true) {
       line = reader.readLine();
@@ -29,7 +28,6 @@ public class Service {
     reader.close();
     return ret;
   }
-
   public Student findStudentByName(String name) {
     return null;
   }
